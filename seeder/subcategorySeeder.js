d11236fb25974d16154a5f5c7cfc674177e9f344
OnescuRadu <onescuradu@yahoo.com>
2020-06-17 14:48:18 +0200

    Updated seed files

--- seeder/subcategorySeeder.js
+++ seeder/subcategorySeeder.js
@@ -39,7 +39,47 @@ module.exports = async () => {
       { title: 'Houses', category: realEstateCat.id },
       { title: 'Lands', category: realEstateCat.id },
       { title: 'Offices', category: realEstateCat.id },
-      { title: 'Other Properties', category: realEstateCat.id }
+      { title: 'Other Properties', category: realEstateCat.id },
+      { title: 'Mobile Phones', category: electronicsAplicancesCat.id },
+      { title: 'Laptops/Computers', category: electronicsAplicancesCat.id },
+      { title: 'TVs', category: electronicsAplicancesCat.id },
+      {
+        title: 'Audio/Video Electronics',
+        category: electronicsAplicancesCat.id
+      },
+      { title: 'Appliances', category: electronicsAplicancesCat.id },
+      { title: 'Tablets/eReaders', category: electronicsAplicancesCat.id },
+      { title: 'Cameras', category: electronicsAplicancesCat.id },
+      { title: 'Gaming Consoles', category: electronicsAplicancesCat.id },
+      {
+        title: 'GPS/Navigation Systems',
+        category: electronicsAplicancesCat.id
+      },
+      {
+        title: 'Other Electronic Devices',
+        category: electronicsAplicancesCat.id
+      },
+      { title: 'Women`s Clothes', category: fashionBeautyCat.id },
+      { title: 'Women`s Shoes', category: fashionBeautyCat.id },
+      { title: 'Men`s Clothes', category: fashionBeautyCat.id },
+      { title: 'Men`s Shoes', category: fashionBeautyCat.id },
+      { title: 'Accessories', category: fashionBeautyCat.id },
+      { title: 'Jewelry', category: fashionBeautyCat.id },
+      { title: 'Cosmetics/Perfumes', category: fashionBeautyCat.id },
+      { title: 'Children`s Clothes', category: fashionBeautyCat.id },
+      { title: 'Children`s Shoes', category: fashionBeautyCat.id },
+      { title: 'Furniture/Decorations', category: houseGardenCat.id },
+      { title: 'Household Items', category: motherChildCat.id },
+      { title: 'Construction Materials', category: houseGardenCat.id },
+      { title: 'Thermal/Electrical/Sanitary', category: houseGardenCat.id },
+      { title: 'Tools/Hardware', category: houseGardenCat.id },
+      { title: 'Sports and Tourism Equipment', category: sportsLeisureArtCat.id },
+      { title: 'Books/Movies/Music', category: sportsLeisureArtCat.id },
+      { title: 'Events/Entertainment', category: sportsLeisureArtCat.id },
+      { title: 'Art/Collectibles', category: sportsLeisureArtCat.id },
+      { title: 'Agricultural and Industrial Equipment', category: agricultureIndustryCat.id },
+      { title: 'Products market - food', category: agricultureIndustryCat.id },
+      { title: 'Cereals/Plants/Trees', category: agricultureIndustryCat.id }
     ];
     Subcategory.create(subcategoryData)
       .then(subcategories => {
